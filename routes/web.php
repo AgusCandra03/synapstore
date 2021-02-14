e459a4867ac9cea9f9e0f051d50296f4440a271d
Candra <aguscandra1995@gmail.com>
2021-02-14 10:53:56 +0700

    CRUD dan validation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home/halo', [HomeController::class, 'halo']);
+// Route::post('/home/store', [HomeController::class, 'halo']);
+Route::resource('barang', BarangController::class);
+// Route::get('/barang/edit_dong', [BarangController::class, 'edit_dong']); 
